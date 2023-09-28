65845a98f57ce1ebed2a6aec9ad9c7b35e884eea
AidanLDev <dev@aidanlowson.com>
2023-09-28 21:30:24 +0100

    Upload to S3

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,10 +1,29 @@
 "use client";
 import { uploadToS3 } from "@/lib/s3";
-import { Inbox } from "lucide-react";
-import React from "react";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader } from "lucide-react";
+import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const FileUpload = () => {
+  const [uploading, setUploading] = useState(false);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const res = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
@@ -13,20 +32,36 @@ const FileUpload = () => {
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
         // Bigger than 10mb!
-        alert("Please upload a smaller file");
+        toast.error("File is too large, must be less than 10mb.");
         return;
       }
 
       try {
+        setUploading(true);
         const data = await uploadToS3(file);
+        if (!data?.file_key || !data?.file_name) {
+          toast.error("Something went wrong uploading the PDF");
+          return;
+        }
+        mutate(data, {
+          onSuccess: () => {
+            toast.success("PDF successfully uploaded");
+          },
+          onError: () => {
+            toast.error("Error creating chat");
+          },
+        });
         console.log("data: ", data);
       } catch (error) {
         console.error("Failed to upload to S3: ", error);
+      } finally {
+        setUploading(false);
       }
     },
   });
   return (
     <div className="p-2 bg-white rounded-xl">
+      {/* TODO: Change styles based on loading state */}
       <div
         {...getRootProps({
           className:
@@ -34,10 +69,17 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
-        <>
-          <Inbox className="w-10 h-10 text-blue-500" />
-          <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
-        </>
+        {uploading || isLoading ? (
+          <>
+            <Loader className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">Uploading PDF</p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          </>
+        )}
       </div>
     </div>
   );
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,48 +1,54 @@
 import AWS from "aws-sdk";
 
-export async function uploadToS3(file: File) {
-  try {
-    const s3 = new AWS.S3({
-      region: "eu-west-2",
-      credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
-      },
-    });
-    console.log("Built S3 obj with: ", {
-      region: "eu-west-2",
-      credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
-      },
-    });
-    const file_key = `uploads/${Date.now().toString()}${file.name.replace(
-      " ",
-      "-"
-    )}`;
-    const params = {
-      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-      Key: file_key,
-      Body: file,
-    };
-    const upload = s3
-      .putObject(params)
-      .on("httpUploadProgress", (evt) => {
-        console.log(
-          "uploading to s3...",
-          parseInt(((evt.loaded * 100) / evt.total).toString()),
-          "%"
-        );
-      })
-      .promise();
+export async function uploadToS3(
+  file: File
+): Promise<{ file_key: string; file_name: string }> {
+  return new Promise((resolve, reject) => {
+    try {
+      const s3 = new AWS.S3({
+        region: "eu-west-2",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
+        },
+      });
+      console.log("Built S3 obj with: ", {
+        region: "eu-west-2",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY!,
+        },
+      });
+      const file_key = `uploads/${Date.now().toString()}${file.name.replace(
+        " ",
+        "-"
+      )}`;
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+        Body: file,
+      };
+      const upload = s3
+        .putObject(params)
+        .on("httpUploadProgress", (evt) => {
+          console.log(
+            "uploading to s3...",
+            parseInt(((evt.loaded * 100) / evt.total).toString()),
+            "%"
+          );
+        })
+        .promise();
 
-    await upload.then((data) => {
-      console.log("Successfully uploaded the PDF to S3!", file_key);
-      return Promise.resolve({ file_key, file_name: file.name });
-    });
-  } catch (error) {
-    console.error("Problem uploading to S3: ", error);
-  }
+      s3.putObject(params, () => {
+        return resolve({
+          file_key,
+          file_name: file.name,
+        });
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 export function getS3Url(file_key: string) {
