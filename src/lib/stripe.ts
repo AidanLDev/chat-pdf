b8ea52986b0318095cb37ea0c1b79db5b581f2c6
AidanLDev <dev@aidanlowson.com>
2024-03-01 08:25:27 +0000

    Updating stripe api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from "stripe";
 
 // STRIPE_TEST_API_KEY STRIPE_API_KEY
 export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
-  apiVersion: "2023-08-16", // TODO: Make this whatever stripe asks us to enter
+  apiVersion: "2023-10-16",
   typescript: true,
 });
