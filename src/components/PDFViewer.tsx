de4df518335220558bdf8b125a0a93ea769aceca
AidanLDev <dev@aidanlowson.com>
2024-03-25 23:05:14 +0000

    Hide pdf viewer IF on mobile

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,15 +1,22 @@
+"use client";
 import React from "react";
+import { useIsMobile } from "breakpoint-hooks";
 
 type Props = {
   pdf_url: string;
 };
 
 const PDFViewer = ({ pdf_url }: Props) => {
+  const isMobile = useIsMobile();
   return (
-    <iframe
-      src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
-      className="w-full h-full"
-    ></iframe>
+    <>
+      {!isMobile && (
+        <iframe
+          src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
+          className="w-full h-full"
+        ></iframe>
+      )}
+    </>
   );
 };
 
